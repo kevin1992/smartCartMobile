0b606f80e9d29515b49d66899ab144f4ecb5c332
kevin <kevin1992ggmail.com>
2017-10-14 17:43:20 -0300

    m

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -3,6 +3,7 @@ import {IonicPage, NavController, NavParams, ToastController, LoadingController}
 import {HomeTabsPage} from "../home-tabs/home-tabs";
 import {ApiService} from "../../services/api.service";
 import {API} from "../../app/app.component";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 /**
  * Generated class for the LoginPage page.
@@ -20,8 +21,24 @@ export class LoginPage {
 
   data: any = {};
   option = 'login';
+  private loginForm: FormGroup;
+  private registerForm: FormGroup;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public api: ApiService,public toastCtrl: ToastController, public loadingCtrl: LoadingController) {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public api: ApiService, public toastCtrl: ToastController, public loadingCtrl: LoadingController, private formBuilder: FormBuilder) {
+
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+
+    this.registerForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      name: ['', Validators.required],
+      last_name: ['', Validators.required]
+    });
+
   }
 
   ionViewDidLoad() {
@@ -35,10 +52,9 @@ export class LoginPage {
 
     loader.present();
 
-    if(window.localStorage.getItem('smartCart-auth')){
+    if (window.localStorage.getItem('smartCart-auth')) {
       this.goMain();
     }
-
 
 
   }
@@ -47,10 +63,14 @@ export class LoginPage {
     this.navCtrl.setRoot(HomeTabsPage);
   }
 
-  login(){
-    this.api.post(API.URL + "login", {email: this.data.user, password: this.data.password},{}).subscribe((data)=>{
+  login() {
 
-      window.localStorage.setItem('smartCart-auth',data.token_type+' '+data.access_token)
+    this.api.post(API.URL + "login", {
+      email: this.loginForm.controls['email'].value,
+      password: this.loginForm.controls['password'].value
+    }, {}).subscribe((data) => {
+
+      window.localStorage.setItem('smartCart-auth', data.token_type + ' ' + data.access_token)
       this.goMain();
 
     });
@@ -58,14 +78,16 @@ export class LoginPage {
 
   register() {
 
-    this.api.post(API.URL + "register", {name: this.data.name, last_name: this.data.last_name,"email": this.data.email,
-      "password": this.data.password}, {
-      successMsg:"El usuario fue creado correctamente"
-    }).subscribe((data)=>{
+    this.api.post(API.URL + "register", {
+      name: this.data.name, last_name: this.data.last_name, "email": this.data.email,
+      "password": this.data.password
+    }, {
+      successMsg: "El usuario fue creado correctamente"
+    }).subscribe((data) => {
 
 
-        this.data = {};
-        this.toggleOption();
+      this.data = {};
+      this.toggleOption();
 
     });
 
